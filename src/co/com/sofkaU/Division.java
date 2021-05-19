f1bb842047c4c419f61108523b9158e642b483ac
Juan Pablo Mesa <tesitolol24@gmail.com>
2021-05-19 11:52:05 -0500

    Se creo el metodo de division

--- src/co/com/sofkaU/Division.java
+++ src/co/com/sofkaU/Division.java
@@ -1,9 +1,13 @@
 package co.com.sofkaU;
 
 public class Division {
-    private int numero1;
-    private int numero2;
+    private double numero1;
+    private double numero2;
     private double resultado;
 
-    
+    public String getDivision(double numero1, double numero2, double resultado){
+        resultado = numero1 / numero2;
+        return "La division es: "+resultado ;
+    }
+
 }
