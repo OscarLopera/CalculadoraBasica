85b8a41deedd23f838ea2589a3c8eead3522e66b
Leonardo Sosa <efrenl.sosab@gmail.com>
2021-05-19 13:31:27 -0500

    se agrego el menu

--- src/co/com/sofkaU/Main.java
+++ src/co/com/sofkaU/Main.java
@@ -1,18 +1,56 @@
 package co.com.sofkaU;
 
 import utils.NumMenor;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args){
 
-    System.out.println("Bienvenidos al menu");
+            Scanner entrada = new Scanner(System.in);
 
-    double n1 = 3, n2= 5.2;
-    double numMenor= NumMenor.buscarNumMenor(n1,n2);
-    System.out.println(numMenor);
+        dibujoMenu();
+        System.out.println("Bienvenidos al menu");
+        System.out.printf("Porfavor digita la opción que quiere ver\nOpcion 1: Factorial\nOpcion 2: Potenciación\nOpcion 3: Dividir\nOpcion 4: Numero menor\n");
+        String opcion = entrada.nextLine();
+
+        switch (opcion){
+            case "1":
+                System.out.printf("Seleccionaste la opción 1");
+
+                break;
+
+            case "2":
+                System.out.printf("Seleccionaste la opción 2");
+                break;
+
+            case "3":
+                System.out.printf("Seleccionaste la opción 3");
+                break;
+
+            case "4":
+                System.out.println("Seleccionaste la opción 4");
+                double n1 = 3, n2= 5.2;
+                double numMenor= NumMenor.buscarNumMenor(n1,n2);
+                System.out.println(numMenor);
+                break;
+
+            default:
+                System.out.printf("Seleccionaste la opción diferente");
+                break;
+
+        }
+
+
+
     //Esto es una prueba;
         // Prueba 2
     }
-
+    private static void dibujoMenu() {
+        System.out.print("\n" +
+                "▐▓█▀▀▀▀▀▀▀▀▀█▓▌░▄▄▄▄▄░\n" +
+                "▐▓█░░▀░░▀▄░░█▓▌░█▄▄▄█░\n" +
+                "▐▓█░░▄░░▄▀░░█▓▌░█▄▄▄█░\n" +
+                "▐▓█▄▄▄▄▄▄▄▄▄█▓▌░█████░\n" +
+                "░░░░▄▄███▄▄░░░░░█████░\n");
+    }
 }
