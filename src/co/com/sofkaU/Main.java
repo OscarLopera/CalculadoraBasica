ed6e8500ebf883c6f33e345a8e5e8f2a556932b3
linazon <linazon94@gmail.comgit>
2021-05-19 11:46:17 -0500

    se agrega funcion numero menor

--- src/co/com/sofkaU/Main.java
+++ src/co/com/sofkaU/Main.java
@@ -1,8 +1,16 @@
 package co.com.sofkaU;
 
+import utils.NumMenor;
+
 public class Main {
 
     public static void main(String[] args){
+
     System.out.println("Bienvenidos al menu");
+
+    double n1 = 3, n2= 5.2;
+    double numMenor= NumMenor.buscarNumMenor(n1,n2);
+    System.out.println(numMenor);
     }
+
 }
