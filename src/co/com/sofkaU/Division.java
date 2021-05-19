d177758ef2b15a392272b300d9d0d40ecd329efd
Juan Pablo Mesa <tesitolol24@gmail.com>
2021-05-19 11:57:05 -0500

    Se agrego al metodo getDivision una validacion que no permita dividir por cero

--- src/co/com/sofkaU/Division.java
+++ src/co/com/sofkaU/Division.java
@@ -5,9 +5,15 @@ public class Division {
     private double numero2;
     private double resultado;
 
+
+
     public String getDivision(double numero1, double numero2, double resultado){
-        resultado = numero1 / numero2;
-        return "La division es: "+resultado ;
+        if(numero1 == 0 || numero2 == 0){
+            return "No se puede dividir por cero";
+        }else {
+            resultado = numero1 / numero2;
+            return "La division es: "+resultado ;
+        }
     }
 
 }
