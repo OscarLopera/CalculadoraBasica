41e03d8d0b3942450b9df8dd36aeb984b78aa37b
Diego Urrego <diegourrg@gmail.com>
2021-05-19 13:21:05 -0500

    Arreglo identacioón método main

--- src/co/com/sofkaU/Main.java
+++ src/co/com/sofkaU/Main.java
@@ -3,7 +3,7 @@ package co.com.sofkaU;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Bienvenidos al menu");
+    System.out.println("Bienvenidos al menu");
 
     }
 }
